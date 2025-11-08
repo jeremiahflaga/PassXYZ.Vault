011a2f85d540774f4051abfafe2c903007e85916
jflaga <flaga.jeremiah@gmail.com>
2025-11-09 04:02:55 +0800

    lifecycle methods defined in the Application class

--- PassXYZ.Vault/App.xaml.cs
+++ PassXYZ.Vault/App.xaml.cs
@@ -43,5 +43,20 @@ namespace PassXYZ.Vault
 
             //return window;
         }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: OnResume");
+        }
     }
 }

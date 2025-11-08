40ff8c31030dbf63fb1697a3d378c2d6667bc269
jflaga <flaga.jeremiah@gmail.com>
2025-11-09 04:03:46 +0800

    add Font-Awsome fonts

--- PassXYZ.Vault/MauiProgram.cs
+++ PassXYZ.Vault/MauiProgram.cs
@@ -13,12 +13,15 @@ namespace PassXYZ.Vault
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
+                    fonts.AddFont("fa-regular-400.ttf", "FontAwesomeRegular");
+                    fonts.AddFont("fa-solid-900.ttf", "FontAwesomeSolid");
+                    fonts.AddFont("fa-brands-400.ttf","FontAwesomeBrands");
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    //fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
